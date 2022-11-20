86c2ec8fd499df98692d1f633a2dc80b7682076b
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-20 17:15:08 -0300

    commit

--- 010/main.js
+++ 010/main.js
@@ -10,14 +10,27 @@ function isString(str) {
     return !/[0-9]/.test(str) && str.length > 1;
 }
 
-question.textContent = "What is your first name?";
+function toCapitalize(str) {
+    if (!(typeof str === "string")) return;
+
+    const words = str.trim().split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] =
+            words[i].charAt(0).toUpperCase() +
+            words[i].slice(1).toLocaleLowerCase();
+    }
+
+    return words.join(" ");
+}
+
+question.textContent = "What is your full name?";
 
 button.addEventListener("click", function () {
     if (!isString(inputName.value)) return;
 
-    msg.textContent = `${textMsg} ${
-        inputName.value.charAt(0).toUpperCase() + inputName.value.slice(1)
-    }!`;
+    msg.textContent = `${textMsg} ${toCapitalize(inputName.value)}!`;
 
+    msg.style.background = "gray";
     msg.style.opacity = 1;
 });
