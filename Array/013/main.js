e39ee44d85d699968fb433479106719262730018
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-24 22:47:57 -0300

    commit

--- Array/013/main.js
+++ Array/013/main.js
@@ -4,17 +4,21 @@ const input = document.getElementById("input");
 const buttonAdd = document.querySelector(".button-add");
 const buttonDisplay = document.querySelector(".button-display");
 const buttonClass = document.getElementsByClassName("button");
+const text = document.getElementById("text");
 const items = [];
 
 // console.log(buttonClass);
 
 buttonAdd.addEventListener("click", function () {
     if (input.value) items.push(Number(input.value));
-    // console.log(items);
 });
 
 buttonDisplay.addEventListener("click", function () {
+    text.textContent = "";
+    let html = "";
+
     items.forEach((value, index, arr) => {
-        console.log(`Element ${index} =  ${value}`);
+        html += `Element ${index} = ${value} \n`;
     });
+    text.textContent = html;
 });
