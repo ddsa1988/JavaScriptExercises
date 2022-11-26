63ed793bc59e4bd3e2cb34a923bda44d23a83ccb
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-26 00:37:09 -0300

    commit

--- Array/016/main.js
+++ Array/016/main.js
@@ -3,3 +3,22 @@
 /*
 Write a JavaScript program to find the leap years in a given range of years.
 */
+
+function isLeapYear(year) {
+    if (!(year % 400)) return true;
+    if (!(year % 100)) return false;
+    if (!(year % 4)) return true;
+
+    return false;
+}
+
+function getLeapYear(yearMin, yearMax) {
+    const leapYears = {};
+    for (let i = yearMin; i <= yearMax; i++) {
+        leapYears[i] = isLeapYear(i);
+    }
+
+    return leapYears;
+}
+
+console.log(getLeapYear(2000, 2020));
