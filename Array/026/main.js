22e20af369d87c5515badbabc4f9240eaf5b5ad2
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-29 22:30:42 -0300

    commit

--- Array/026/main.js
+++ Array/026/main.js
@@ -6,3 +6,14 @@ from an given array whose sum equals a specific target number.
 Input: numbers= [10,20,10,40,50,60,70], target=50
 Output: 2, 3
 */
+
+function sum(arr, target) {
+    if (!Array.isArray(arr) || !arr.length) throw "Invalid Array!";
+    if (!Number.isFinite(target)) throw "Invalid Number!";
+
+    const indices = new Array();
+}
+
+const sampleArray = [10, 20, 10, 40, 50, 60, 70];
+
+console.log(sum(sampleArray, 50));
