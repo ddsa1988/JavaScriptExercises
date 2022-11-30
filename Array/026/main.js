c3d51923ae349c03f8573835023e1411144267c0
Diego Alexandre <ddsa.1988@outlook.com>
2022-11-30 00:01:38 -0300

    commit

--- Array/026/main.js
+++ Array/026/main.js
@@ -8,12 +8,28 @@ Output: 2, 3
 */
 
 function sum(arr, target) {
-    if (!Array.isArray(arr) || !arr.length) throw "Invalid Array!";
+    if (!Array.isArray(arr) || arr.length < 2) throw "Invalid Array!";
     if (!Number.isFinite(target)) throw "Invalid Number!";
 
     const indices = new Array();
+    let pairFound = false;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                indices.push(i, j);
+                pairFound = true;
+                break;
+            }
+        }
+        if (pairFound) break;
+    }
+
+    return indices;
 }
 
 const sampleArray = [10, 20, 10, 40, 50, 60, 70];
 
 console.log(sum(sampleArray, 50));
+console.log(sum(sampleArray, 100));
+console.log(sum(sampleArray, 90));
