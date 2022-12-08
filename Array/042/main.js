0ef13aed975656fe634cb7a0173f2e736c032310
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-08 00:14:56 -0300

    commit

--- Array/042/main.js
+++ Array/042/main.js
@@ -8,3 +8,18 @@ console.log(difference([1, 2, 3], [100, 2, 1, 10])); ["1", "2", "3", "10", "100"
 console.log(difference([1, 2, 3, 4, 5], [1, [2], [3, [[4]]],[5,6]])); ["1", "2", "3", "4", "5", "6"]
 console.log(difference([1, 2, 3], [100, 2, 1, 10])); ["1", "2", "3", "10", "100"]
 */
+
+function uniqueElements(arr1, arr2) {
+    if (!Array.isArray(arr1) || !arr1.length)
+        throw TypeError("Invalid Array 1!");
+    if (!Array.isArray(arr2) || !arr2.length)
+        throw TypeError("Invalid Array 2!");
+
+    const newArray = arr1.flat(Infinity).concat(arr2.flat(Infinity));
+
+    return [...new Set(newArray)].sort((a, b) => a - b);
+}
+
+console.log(uniqueElements([1, 2, 3], [100, 2, 1, 10]));
+console.log(uniqueElements([1, 2, 3, 4, 5], [1, [2], [3, [[4]]], [5, 6]]));
+console.log(uniqueElements([1, 2, 3], [100, 2, 1, 10]));
