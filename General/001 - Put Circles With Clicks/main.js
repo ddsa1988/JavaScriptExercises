773f368a5066043bf87f7d3c08c961e4b5645b45
Diego Alexandre <ddsa.1988@outlook.com>
2022-12-27 00:04:33 -0300

    commit

---
+++ General/001 - Put Circles With Clicks/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/*
+A cada click na tela deve ser inserido um círculo na posição exata do click.
+Inserir um botão de desfazer para apagar o círculo adicionado e um botão de 
+refazer para adicionar o círculo novamente.
+*/
